cca68d9a950c0c7084233c616fcaf20fd3d1076f
Breno <breno.csilva@hotmail.com>
2020-03-27 17:43:52 -0300

    backend Node

--- src/app/funcionario.service.ts
+++ src/app/funcionario.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class FuncionarioService {
 
-  constructor() { }
+  uri = '/funcionario';
+
+  constructor(private http: HttpClient) { }
+
+  adicionarFuncionario(nomeFuncionario, cargo, numeroIdentificador){
+    const objFuncionario={
+    nomeFuncionario:nomeFuncionario,
+    cargo:cargo,
+    numeroIdentificador:numeroIdentificador
+  };
+  console.log(objFuncionario);
+  this.http.post(`${this.uri}/add`,objFuncionario)
+  .subscribe(res => console.log('ok'));
+   }
 }
